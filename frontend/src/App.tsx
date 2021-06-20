45a0126df1fef8c7de4c8d7d7430c3fec125155b
Dominique Metz <DominiqueMetz@gmx.de>
2021-06-20 11:59:08 +0200

    Remove unnecessary dependency. Fixup logos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import Channels from "./pages/channels/channels";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Helmet} from "react-helmet";
 
 const App = () => {
-    return <>
-        <Helmet>
-            <meta charSet="utf-8"/>
-            <title>TS-Dashboard</title>
-        </Helmet>
-        <Channels/>;
-    </>
+    return <Channels/>;
 }
 
 export default App;
